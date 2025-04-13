900004c2badb0d14dd00376e4672684476ace711
= <=>
2025-04-12 23:32:42 -0300

    feat: validator dto

--- api/src/modules/users/dto/create-users-input.ts
+++ api/src/modules/users/dto/create-users-input.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsString, IsNumber } from 'class-validator';
+
 export default class CreateUsersInput {
+    @IsString()
     name: string;
+
+    @IsNumber()
     account_balance: number;
+
+    @IsString()
+    @IsEmail()
     email: string;
 }
